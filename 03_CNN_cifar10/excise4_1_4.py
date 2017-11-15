f60d8592536f602967e40c1a6b4a2004f76821d8
fmsunyh <fmsunyh@gmail.com>
2017-11-15 17:55:00 +0800

    summary graph image and so on.

--- 03_CNN_cifar10/excise4_1_4.py
+++ 03_CNN_cifar10/excise4_1_4.py
@@ -10,7 +10,7 @@ import urllib
 
 import tensorflow as tf
 import os
-import  numpy as np
+import numpy as np
 import csv
 import sys
 
@@ -20,6 +20,7 @@ import cifar10_input
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 
 def get_session():
     cfg = tf.ConfigProto()
@@ -48,36 +49,40 @@ fc2_units_num = 192
 
 datast_dir = '../data/cifar/'
 
+
 def maybe_download_and_extract(data_dir):
-    """下载并解压数据集from Alex's website."""
+    """download from Alex's website."""
     dest_directory = data_dir
     DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
     if not os.path.exists(dest_directory):
         os.makedirs(dest_directory)
     filename = DATA_URL.split('/')[-1]
-    filepath = os.path.join(dest_directory,filename)
+    filepath = os.path.join(dest_directory, filename)
     if not os.path.exists(filepath):
-        def _progress(count,block_size,total_size):
-            sys.stdout.write('\r>>Downloading %s %.1f%%'%(filename,
-                                                          float(count*batch_size)/float(total_size)*100.0))
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>>Downloading %s %.1f%%' % (filename,
+                                                            float(count * batch_size) / float(total_size) * 100.0))
             tf.sys.stdout.flush()
-        filepath,_ = urllib.request.urlretrieve(DATA_URL,filepath,_progress)
+
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
         print()
         statinfo = os.stat(filepath)
-        print('Successfully downloaded',filename,statinfo.st_size,'bytes.')
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
 
-    tarfile.open(filepath,'r:gz').extractall(dest_directory)
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
 
 def get_distorted_train_batch(data_dir, batch_size):
-    #data_dir='data/cifar/'
+    # data_dir='data/cifar/'
 
     if not data_dir:
         raise ValueError('Please supply a data_dir')
 
     data_dir = os.path.join(data_dir, 'cifar-10-batches-bin')
 
-    images, labels = cifar10_input.distorted_inputs(data_dir=data_dir,batch_size=batch_size)
+    images, labels = cifar10_input.distorted_inputs(data_dir=data_dir, batch_size=batch_size)
     return images, labels
+
 
 def get_undistorted_eval_batch(eval_data, data_dir, batch_size):
     if not data_dir:
@@ -89,54 +94,78 @@ def get_undistorted_eval_batch(eval_data, data_dir, batch_size):
     return images, labels
 
 
-
 def get_undistorted_train_batch(eval_data, data_dir, batch_size):
     pass
 
 
-def WeightsVariable(shape, name_str, stddev = 0.1):
-    initial = tf.truncated_normal(shape=shape,stddev=stddev, dtype=tf.float32)
-    return tf.Variable(initial,dtype=tf.float32, name=name_str)
+def WeightsVariable(shape, name_str, stddev=0.1):
+    initial = tf.truncated_normal(shape=shape, stddev=stddev, dtype=tf.float32)
+    return tf.Variable(initial, dtype=tf.float32, name=name_str)
+
 
 def BiasesVariable(shape, name_str, init_value=0.0):
     initial = tf.constant(init_value, shape=shape)
     return tf.Variable(initial, dtype=tf.float32, name=name_str)
 
-def Conv2(x, W , b, stride=1, padding='SAME', activation=tf.nn.relu, act_name='relu'):
+
+def Conv2(x, W, b, stride=1, padding='SAME', activation=tf.nn.relu, act_name='relu'):
     with tf.name_scope('conv2_bias'):
-        y = tf.nn.conv2d(x,W, strides=[1,stride ,stride,1],padding=padding)
-        y = tf.nn.bias_add(y,b)
+        y = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding=padding)
+        y = tf.nn.bias_add(y, b)
 
     with tf.name_scope(act_name):
         y = activation(y)
-    return  y
+    return y
+
 
 def Pool2d(x, pool=tf.nn.max_pool, k=2, stride=2, padding='SAME'):
-    return  pool(x, ksize=[1,k,k,1], strides=[1,stride, stride, 1], padding='SAME')
+    return pool(x, ksize=[1, k, k, 1], strides=[1, stride, stride, 1], padding='SAME')
+
 
 def FullyConnected(x, W, b, activate=tf.nn.relu, act_name='relu'):
     with tf.name_scope('Wx_b'):
-        y = tf.matmul(x,W)
-        y = tf.add(y,b)
+        y = tf.matmul(x, W)
+        y = tf.add(y, b)
 
     with tf.name_scope(act_name):
         y = activate(y)
 
     return y
 
+
+def AddActivationSummary(x):
+    tf.summary.histogram('/activations', x)
+    tf.summary.scalar('/sparsity', tf.nn.zero_fraction(x))
+
+
+def AddLossesSummary(losses):
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    loss_averages_op = loss_averages.apply(losses)
+
+    for loss in losses:
+        tf.summary.scalar(loss.op.name + '(raw)', loss)
+        tf.summary.scalar(loss.op.name + 'avg', loss_averages.average(loss))
+
+    return loss_averages_op
+
+
 def Inference(images_holder):
     with tf.name_scope('Conv2d_1'):
-        weights = WeightsVariable(shape=[5,5, image_channel, conv1_kernel_num], name_str='weights', stddev=5e-2)
+        weights = WeightsVariable(shape=[5, 5, image_channel, conv1_kernel_num], name_str='weights', stddev=5e-2)
         biases = BiasesVariable(shape=[conv1_kernel_num], name_str='baises', init_value=0.0)
         conv1_out = Conv2(images_holder, weights, biases, stride=1, padding='SAME')
+
+        AddActivationSummary(conv1_out)
 
     with tf.name_scope('Pool2d_1'):
         pool1_out = Pool2d(conv1_out, pool=tf.nn.max_pool, k=3, stride=2, padding='SAME')
 
     with tf.name_scope('Conv2d_2'):
-        weights = WeightsVariable(shape=[3,3, conv1_kernel_num, conv2_kernel_num], name_str='weights', stddev=5e-2)
+        weights = WeightsVariable(shape=[3, 3, conv1_kernel_num, conv2_kernel_num], name_str='weights', stddev=5e-2)
         biases = BiasesVariable(shape=[conv2_kernel_num], name_str='baises', init_value=0.0)
         conv2_out = Conv2(pool1_out, weights, biases, stride=1, padding='SAME')
+
+        AddActivationSummary(conv2_out)
 
     with tf.name_scope('Pool2d_2'):
         pool2_out = Pool2d(conv2_out, pool=tf.nn.max_pool, k=3, stride=2, padding='SAME')
@@ -149,8 +178,9 @@ def Inference(images_holder):
         weights = WeightsVariable(shape=[feats_dim, fc1_units_num], name_str='weights', stddev=4e-2)
         biases = BiasesVariable(shape=[fc1_units_num], name_str='biases', init_value=0.1)
 
-        fc1_out = FullyConnected(features, weights,biases,activate=tf.nn.relu, act_name='relu')
+        fc1_out = FullyConnected(features, weights, biases, activate=tf.nn.relu, act_name='relu')
 
+        AddActivationSummary(fc1_out)
 
     with tf.name_scope('FC2_nolinear'):
         weights = WeightsVariable(shape=[fc1_units_num, fc2_units_num], name_str='weights', stddev=4e-2)
@@ -158,12 +188,16 @@ def Inference(images_holder):
 
         fc2_out = FullyConnected(fc1_out, weights, biases, activate=tf.nn.relu, act_name='relu')
 
+        AddActivationSummary(fc2_out)
+
     with tf.name_scope('FC3_linear'):
         fc3_units_num = n_classes
-        weights = WeightsVariable(shape=[fc2_units_num, fc3_units_num], name_str='weights', stddev=1.0/fc3_units_num)
+        weights = WeightsVariable(shape=[fc2_units_num, fc3_units_num], name_str='weights', stddev=1.0 / fc3_units_num)
         biases = BiasesVariable(shape=[fc3_units_num], name_str='biases', init_value=0.0)
 
         logits = FullyConnected(fc2_out, weights, biases, activate=tf.identity, act_name='linear')
+
+        AddActivationSummary(logits)
 
     return logits
 
@@ -183,6 +217,8 @@ def TrainModel():
             cross_entropy_mean = tf.reduce_mean(cross_entropy)
 
             total_loss = cross_entropy_mean
+
+            average_losses = AddLossesSummary([total_loss])
 
         with tf.name_scope('Train'):
             learning_rate = tf.placeholder(tf.float32)
@@ -211,19 +247,19 @@ def TrainModel():
         init_op = tf.global_variables_initializer()
 
         print('save the graph')
-        graph_writer = tf.summary.FileWriter(logdir='logs/excise413', graph=tf.get_default_graph())
-        graph_writer.close()
-
+        summary_writer = tf.summary.FileWriter(logdir='logs/excise414')
+        summary_writer.add_graph(graph=tf.get_default_graph())
+        summary_writer.flush()
 
         results_list = list()
-        results_list.append(['learning_rate',learning_rate_value,
-                             'training_epochs',training_epochs,
-                             'batch_size',batch_size,
-                             'display_step',display_step,
-                             'conv1_kernel_num',conv1_kernel_num,
-                             'conv2_kernel_num',conv2_kernel_num,
-                             'fc1_units_num',fc1_units_num,
-                             'fc2_units_num',fc2_units_num])
+        results_list.append(['learning_rate', learning_rate_value,
+                             'training_epochs', training_epochs,
+                             'batch_size', batch_size,
+                             'display_step', display_step,
+                             'conv1_kernel_num', conv1_kernel_num,
+                             'conv2_kernel_num', conv2_kernel_num,
+                             'fc1_units_num', fc1_units_num,
+                             'fc2_units_num', fc2_units_num])
 
         results_list.append(['train_step', 'train_loss', 'train_step', 'train_accuracy'])
 
@@ -259,13 +295,19 @@ def TrainModel():
                                                       labels_holder: labels_batch}
                                            )
 
-                    batch_acc = np.sum(predictions)*1.0 / batch_size
+                    batch_acc = np.sum(predictions) * 1.0 / batch_size
                     print('Training step:' + str(training_step)
                           + ', Training Loss=' + '{:.6f}'.format(loss_value)
                           + ', Training Accuracy=' + '{:.5f}'.format(batch_acc)
                           )
 
                     results_list.append([training_step, loss_value, training_step, batch_acc])
+                    summary_str = sess.run(merged_summaries,
+                                           feed_dict={images_holder: images_batch, labels_holder: labels_batch})
+                    summary_writer.add_summary(summary=summary_str, global_step=training_step)
+                    summary_writer.flush()
+
+            summary_writer.close()
         print('finish training......')
 
         print('==>>>>>==start testing==<<<<<==')
@@ -284,7 +326,7 @@ def TrainModel():
                                               labels_holder: labels_batch}
                                    )
             correct_predicted += np.sum(predictions)
-        accuracy_score = correct_predicted*1.0 / total_exmples
+        accuracy_score = correct_predicted * 1.0 / total_exmples
         print("Accuracy on Test Exmples:", accuracy_score)
         results_list.append(['Accuracy on Test Examples: ', accuracy_score])
 
@@ -299,6 +341,7 @@ def TrainModel():
         coord.join(threads)
         sess.close()
 
+
 def main(argv=None):
     # maybe_download_and_extract(data_dir=datast_dir)
 
@@ -309,5 +352,6 @@ def main(argv=None):
 
     TrainModel()
 
+
 if __name__ == '__main__':
-    tf.app.run(main=main,argv=None)
+    tf.app.run(main=main, argv=None)
