47afbe32e3ce7276b93dc85c06f881e3ca85c6fc
fmsunyh <fmsunyh@gmail.com>
2017-11-15 18:00:51 +0800

    Funciton: read the dataset of cifar100 Provide the read function for cifar100 base on cifar10 which we have finish before. Intialize the cifardataset. Are you ready.

---
+++ cifardataset/cifar_input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 11/15/2017 5:57 PM 
+# @Author : sunyonghai 
+# @File : cifar_input.py 
+# @Software: BG_AI
+# =========================================================
